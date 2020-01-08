6655946a1db8e8123102b4b9726923ffb52e0d3c
av1200 <ggz521@126.com>
2020-01-08 10:29:44 +0800

    first

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img src="./assets/img/logo.png" />
+    
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About</router-link>
+    
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App"
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  background-image: url("./assets/img/body.jpg")
+}
+</style>
---
+++ src/components/about.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    This is about vue
+    <!-- <ul>
+      <li v-for="(item,index) in items" :key="index">
+        <img :src="item.imgsrc">{{index}}{{item.imgsrc}}
+        </li>
+    </ul>-->
+    <div v-for="(item,index) in items" :key="index" class="a">
+      <img :src="item.imgsrc" />
+      {{index}}{{item.imgsrc}}
+    </div>
+
+    <P>
+      <button v-on:click="getImg"  >getImg</button>
+    </P>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "about",
+  data() {
+    return {
+      items: [
+        // { title: "图A", imgsrc: require("@/assets/img/timg1.png") },
+        // { title: "图B", imgsrc: require("@/assets/img/timg2.png") },
+        // { title: "图B", imgsrc: require("@/assets/img/timg3.png") }
+      ]
+    };
+  },
+  methods: {
+    getImg() {
+       console.log("porcess getImg");
+        var x1=1
+        var x2=2 
+        var x3=3;
+        this.items = [
+           { title: "图A", imgsrc: require("@/assets/img/timg"+x1+".png") },
+           { title: "图B", imgsrc: require("@/assets/img/timg"+x2+".png") },
+           { title: "图B", imgsrc: require("@/assets/img/timg"+x3+".png") }
+        ];
+     
+    }
+  }
+};
+</script>
+
+<style scoped>
+.a {
+  display: block;
+}
+</style>
